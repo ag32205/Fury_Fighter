c09d76940f777ba188c68d05a07d93fcecf5ed54
Max Lobato <MaxLobato@Max-Lobatos-MBP.attlocal.net>
2018-04-17 16:28:49 -0700

    trying stuff

--- Fury_Fighters.js
+++ Fury_Fighters.js
@@ -1,11 +1,7 @@
-document.body.onkeydown = function(e) {
-    if (e.keyCode === 32) {
+document.body.onkeydown = function (e) {
+    if (e.keyCode === 65) {
         var player1 = document.getElementById('player1');
-        var width = player1.style.width;
-        width =
+        var width = new String(player1.style.width.valueOf);
+        player1.style.width = (width.slice(1, 3) - 25) + 'px';
     }
 }
-
-
-
-
