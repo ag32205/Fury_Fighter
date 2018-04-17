42d6d867fc14cf8dc3c9e8b54137acd6a998ba10
Max W Lobato <ml93117@srcs.loc>
2018-04-17 11:42:09 -0700

    lotta stuff

---
+++ Fury_Fighters.js
@@ -0,0 +1,13 @@
+var player1 = document.getElementById('player1');
+
+player1.addEventListener("keydown", ('e') => {
+
+    if (e.KeyCode == 32){
+        player1.className.value = "player1";
+    }
+
+} )
+
+
+
+
