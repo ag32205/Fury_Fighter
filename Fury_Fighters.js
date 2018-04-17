6ecc74bac85360b8878c1ac6c45a4f3c6d7d4382
Max W Lobato <ml93117@srcs.loc>
2018-04-17 12:20:51 -0700

    ewrvbfd

--- Fury_Fighters.js
+++ Fury_Fighters.js
@@ -1,7 +1,8 @@
 document.body.onkeydown = function(e) {
     if (e.keyCode === 32) {
         var player1 = document.getElementById('player1');
-        player1.style.width = "100px";
+        var width = player1.style.width;
+        width =
     }
 }
 
