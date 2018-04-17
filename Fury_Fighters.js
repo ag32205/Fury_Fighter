6bb3f21d1d79b3efedaa53acad07749113f7fb36
Max W Lobato <ml93117@srcs.loc>
2018-04-17 12:04:07 -0700

    HE CAN MOVE sorta

--- Fury_Fighters.js
+++ Fury_Fighters.js
@@ -1,12 +1,9 @@
-var player1 = document.getElementById('player1');
-
-player1.addEventListener("keydown", ('e') => {
-
-    if (e.KeyCode == 32){
-        player1.className.value = "player1";
+document.body.onkeydown = function(e) {
+    if (e.keyCode === 32) {
+        var player1 = document.getElementById('player1');
+        player1.style.width = "100px";
     }
-
-} )
+}
 
 
 
