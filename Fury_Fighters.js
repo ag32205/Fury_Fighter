275034203d50294b7e67b07e06950c1bd9272104
Max W Lobato <ml93117@srcs.loc>
2018-04-19 11:12:07 -0700

    finally not overwatch hitbox

--- Fury_Fighters.js
+++ Fury_Fighters.js
@@ -1,7 +1,9 @@
 document.body.onkeydown = function (e) {
     if (e.keyCode === 65) {
         var player1 = document.getElementById('player1');
-        var width = new String(player1.style.width.valueOf);
-        player1.style.width = (width.slice(1, 3) - 25) + 'px';
+        var width = new String(player1.style.width);
+        player1.style.width.valueOf = function () {
+            width = ((width.slice(1, 3)) - 25) + 'px';
+        }
     }
 }
