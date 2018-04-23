55502a6526dfacda6aeec40950ca68dc5a680f0c
Max W Lobato <ml93117@srcs.loc>
2018-04-23 11:11:47 -0700

    moves

--- Fury_Fighters.js
+++ Fury_Fighters.js
@@ -5,21 +5,24 @@ function Player(pos, speed, controlset, id) {
     this.id = id;
     var ypos = 570;
     var fighter = document.getElementById(this.id);
-    var position = this.pos;
 
-    fighter.style.left = position + 'px';
+    this.move = function () {
+        fighter.style.left = this.pos + 'px';
+        fighter.style.top = ypos + 'px';
+    }
+    fighter.style.left = this.pos + 'px';
     fighter.style.top = ypos + 'px';
 }
 
-var Player1 = new Player(400, 25, 1, 'player1');
+var Player1 = new Player(400, 5, 1, 'player1');
 
 
-document.Player1.onkeydown = function (e) {
-        if (e.keyCode === 65) {
-            Player1.pos = Player1.pos - Player1.speed;
-        }
-        if (e.keyCode === 37) {
-            position = position - Player1.speed;
-        }
-      return position;
-    };
+Player1.onkeypress = function (e) {
+    if (e.keyCode === 65) {
+        Player1.pos = Player1.pos - Player1.speed;
+    }
+    if (e.keyCode === 37) {
+        Player1.pos = Player1.pos - Player1.speed;
+    }
+    Player1.move();
+};
