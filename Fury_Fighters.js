bac5e38f89a348e110ec923bb0f55ac551477f62
Abraham  Gonzalez <ag32205@srcs.loc>
2018-04-25 11:39:28 -0700

    y pos

--- Fury_Fighters.js
+++ Fury_Fighters.js
@@ -3,20 +3,19 @@ function Player(pos, speed, controlset, id) {
     this.speed = speed;
     this.controlset = controlset;
     this.id = id;
-    var ypos = 570;
+    this.ypos = 570;
     var fighter = document.getElementById(this.id);
 
     this.move = function () {
         fighter.style.left = this.pos + 'px';
-        fighter.style.top = ypos + 'px';
+        fighter.style.top = this.ypos + 'px';
     }
     fighter.style.left = this.pos + 'px';
-    fighter.style.top = ypos + 'px';
+    fighter.style.top = this.ypos + 'px';
 }
 
 var Player1 = new Player(400, 5, 1, 'player1');
 var iskeydown = Boolean;
-
 
 function run(e) {
     if (iskeydown === true) {
@@ -31,6 +30,18 @@ function run(e) {
         }
         if (e.keyCode === 39 && Player1.pos < 780) {
             Player1.pos = Player1.pos + Player1.speed;
+        }
+         if (e.keyCode === 38 && Player1.ypos > -11) {
+            Player1.ypos = Player1.ypos - Player1.speed;
+        }
+         if (e.keyCode === 87 && Player1.ypos > -11) {
+            Player1.ypos = Player1.ypos - Player1.speed;
+        }
+         if (e.keyCode === 40 && Player1.ypos < 570) {
+            Player1.ypos = Player1.ypos + Player1.speed;
+        }
+         if (e.keyCode === 83 && Player1.ypos < 570) {
+            Player1.ypos = Player1.ypos + Player1.speed;
         }
         Player1.move();
     }
