640df5c63d52da72a7cf98e46eccd3a8b8143fdd
Max Lobato <MaxLobato@Max-Lobatos-MBP.attlocal.net>
2018-04-25 17:05:54 -0700

    ok mac was being weird

--- Fury_Fighters.js
+++ Fury_Fighters.js
@@ -1,51 +1,88 @@
+
 function Player(pos, speed, controlset, id) {
     this.pos = pos;
     this.speed = speed;
     this.controlset = controlset;
     this.id = id;
-    var ypos = 570;
+    this.ypos = 570;
     var fighter = document.getElementById(this.id);
 
     this.move = function () {
         fighter.style.left = this.pos + 'px';
-        fighter.style.top = ypos + 'px';
-    }
+        fighter.style.top = this.ypos + 'px';
+    };
+
     fighter.style.left = this.pos + 'px';
-    fighter.style.top = ypos + 'px';
+    fighter.style.top = this.ypos + 'px';
 }
 
-var Player1 = new Player(400, 5, 1, 'player1');
+function Keyset(left, right, up) {
+    this.left = left;
+    this.right = right;
+    this.up = up;
+    this.moving = Boolean;
+}
+
+var keys = new Keyset(37, 39, 87);
+
+var Player1 = new Player(400, 2, 1, 'player1');
 var iskeydown = Boolean;
 
 
-function run(e) {
-    if (this.iskeydown === true) {
-        if (e.keyCode === 65 && Player1.pos > -10) {
-            Player1.pos = Player1.pos - Player1.speed;
-        }
-        if (e.keyCode === 37 && Player1.pos > -10) {
-            Player1.pos = Player1.pos - Player1.speed;
-        }
-        if (e.keyCode === 68 && Player1.pos < 780) {
-            Player1.pos = Player1.pos + Player1.speed;
-        }
-        if (e.keyCode === 39 && Player1.pos < 780) {
-            Player1.pos = Player1.pos + Player1.speed;
-        }
+function run() {
+    if (keys.moving == true){
+    if (keys.left === true && Player1.pos > -10) {
+        Player1.pos = Player1.pos - Player1.speed;
+    }
+    if (keys.right === true && Player1.pos > -10) {
+        Player1.pos = Player1.pos - Player1.speed;
+    }
+    if (keys.right === true && Player1.pos < 780) {
+        Player1.pos = Player1.pos + Player1.speed;
+    }
+    if (keys.right === true&& Player1.pos < 780) {
+        Player1.pos = Player1.pos + Player1.speed;
+    }
+      Player1.move();
+    }
 
-    } else{}
-    Player1.move();
 }
+var framerate = setInterval(run, 1);
 
 document.body.onkeydown = function frame(e) {
-    iskeydown = true;
-    run(e);
-}
+    keys.moving = true;
+
+    if (e.keyCode === 65) {
+        keys.left = true;
+        keys.right = false;
+    }
+    if (e.keyCode === 37) {
+        keys.left = true;
+        keys.right = false;
+    }
+    if (e.keyCode === 68) {
+        keys.right = true;
+        keys.left = false;
+    }
+    if (e.keyCode === 39) {
+        keys.right = true;
+        keys.left = false;
+    }
+    if (e.keyCode === 38 && Player1.ypos > -11) {
+        keys.up = true;
+    }
+    if (e.keyCode === 87 && Player1.ypos > -11) {
+        keys.up = true
+    }
+
+
+};
 
 
 
 
 document.body.onkeyup = function (e) {
-    iskeydown = false;
-    run(e);
-}
+    keys.moving = false;
+};
+
+window.addEventListener('keydown', run);
