14dddec6a73ce76ebeb8f9dc9135b1479faae4eb
Max Lobato <MaxLobato@Max-Lobatos-MBP.attlocal.net>
2018-04-25 23:43:53 -0700

    experimenting with animations

--- Fury_Fighters.js
+++ Fury_Fighters.js
@@ -1,4 +1,3 @@
-
 function Player(pos, speed, controlset, id) {
     this.pos = pos;
     this.speed = speed;
@@ -27,27 +26,59 @@ var keys = new Keyset(37, 39, 87);
 
 var Player1 = new Player(400, 2, 1, 'player1');
 var iskeydown = Boolean;
+var img = document.getElementById('player1');
+var src = document.createAttribute('src');
 
+img.setAttributeNode(src);
 
 function run() {
-    if (keys.moving === true){
-    if (keys.left === true && Player1.pos > -10) {
-        Player1.pos = Player1.pos - Player1.speed;
-    }
-    if (keys.right === true && Player1.pos > -10) {
-        Player1.pos = Player1.pos - Player1.speed;
-    }
-    if (keys.right === true && Player1.pos < 780) {
-        Player1.pos = Player1.pos + Player1.speed;
-    }
-    if (keys.right === true&& Player1.pos < 780) {
-        Player1.pos = Player1.pos + Player1.speed;
-    }
-      Player1.move();
+    if (keys.moving === true) {
+        if (keys.left === true && Player1.pos > -10) {
+            Player1.pos = Player1.pos - Player1.speed;
+        }
+        if (keys.right === true && Player1.pos > -10) {
+            Player1.pos = Player1.pos - Player1.speed;
+        }
+        if (keys.right === true && Player1.pos < 780) {
+            Player1.pos = Player1.pos + Player1.speed;
+        }
+        if (keys.right === true && Player1.pos < 780) {
+            Player1.pos = Player1.pos + Player1.speed;
+        }
+        Player1.move();
     }
 
 }
 var framerate = setInterval(run, 1);
+
+function animate() {
+
+    for (i = 0; i > 8; i += 0.5) {
+        if (i === 1) {
+            img.src.value = "sprite_red/practice_attack_2.png";
+        }
+        if (i === 2) {
+            img.src.value = "sprite_red/practice_attack_3.png";
+        }
+        if (i === 3) {
+            img.src.value = "sprite_red/practice_attack_4.png";
+        }
+        if (i === 4) {
+            img.src.value = "sprite_red/practice_attack_5.png";
+        }
+        if (i === 5) {
+            img.src.value = "sprite_red/practice_attack_6.png";
+        }
+        if (i === 6) {
+            img.src.value = "sprite_red/practice_attack_7.png";
+        }
+        if (i === 7) {
+            img.src.value = "sprite_red/practice_attack_7.png";
+        }
+    }
+}
+
+var framerateattack = setInterval(animate, 5);
 
 document.body.onkeydown = function frame(e) {
     keys.moving = true;
@@ -68,10 +99,12 @@ document.body.onkeydown = function frame(e) {
         keys.right = true;
         keys.left = false;
     }
-
-
+    if (e.keyCode === 32) {
+        animate();
+    }
 
 };
+
 
 
 
