4def81ebf95f9ddebe9e08b5ec832ee2432e15a1
Max Lobato <MaxLobato@Max-Lobatos-MBP.attlocal.net>
2018-04-26 23:48:34 -0700

    the animations work now

--- Fury_Fighters_animation.js
+++ Fury_Fighters_animation.js
@@ -3,12 +3,33 @@ var src = document.createAttribute('src');
 src.value = "sprite_red/practice_idle.png";
 img.setAttributeNode(src);
 
-function animate() {
+function animate(e) {
 
-src.value = "sprite_red/practice_attack_4.png";
+    if (e.keyCode === 32) {
 
+        setTimeout(function () {
+            src.value = "sprite_red/practice_attack_2.png";
+        }, 1);
+        setTimeout(function () {
+            src.value = "sprite_red/practice_attack_3.png";
+        }, 100);
+        setTimeout(function () {
+            src.value = "sprite_red/practice_attack_4.png";
+        }, 200);
+        setTimeout(function () {
+            src.value = "sprite_red/practice_attack_5.png";
+        }, 300);
+        setTimeout(function () {
+            src.value = "sprite_red/practice_attack_6.png";
+        }, 400);
+        setTimeout(function () {
+            src.value = "sprite_red/practice_attack_7.png";
+        }, 500);
+        setTimeout(function () {
+            src.value = "sprite_red/practice_idle.png";
+        }, 600);
+    }
 }
-
 
 
 
